1b7a88bce1903f613aad352796d0408a201e79a7
momiji-yamamoto <momiji_yamamoto@dwango.co.jp>
2016-05-31 12:51:42 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,286 @@
+var allroot={
+//正規ルート
+"novel": [
+{
+	"message":"<p class='animated fadeInUp'>学校から帰宅すると、妹がリビングで電話をしているところだった。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>妹の名前は、<ruby>高坂<rt>こうさか</rt>桐乃<rt>きりの</rt></ruby>。現在十四歳。近所の中学校にかよっている女子中学生だ。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>ライトブラウンに染めた髪の毛、両耳にはピアス、長くのばした爪には艶やかにマニキュアを塗っている。 </p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>すっぴんでも十分目を惹くだろう端正な顔を、入念なメイクでさらに磨き上げている。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>中学生には見えないくらい大人びた雰囲気。背がすらっと高く、しかし出るところはきっちり出ている─。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>これで歌でも上手ければ、いかにも女受けしそうなカリスマアイドルのでき上がりだ。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>身内の<ruby>贔屓目<rt>ひいきめ</rt></ruby>なんかじゃない。俺の妹は、とにかく垢抜けているやつなのだ。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>もっとも自慢の妹だと誇るつもりはぜんぜんない。男連中からはよく羨ましがられるし、連中の気持ちも分からんでもないが、俺としては冗談じゃないと言いたいね。</p>"	
+},
+{
+	"message":"<p class='animated fadeInUp'>実際に妹がいるやつなら、ちょっとは俺の気持ちが分かってくれるんじゃないかと思う。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>妹ってのは、そんなにいいもんじゃない。少なくとも俺にとっては。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>例えばこう考えてみてくれ。学校のクラスには、たいてい幾つかの友達グループがあるよな。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>その中でも一番華やかなグループを思い浮かべてみるんだ。運動部のエースやら、秀才のイケメンやら、特別かわいい女子なんかが中心になってる集団さ。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>その集団の中でも、さらに一段、垢抜けている女子。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>なんだか話しかけるのも躊躇しちまうような、今後もずっと関わることのないだろう、別世界の住人。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>いわゆる『高めの女子』ってやつだ。見てくれがどんなによかろうと、たいていの男なら、苦手なタイプだって思うよな。俺もそうさ。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>そんな女が、自分の家族だと想像してみろ。もちろんお互いの距離感は据え置きで、だ。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>……どうだ、分かるか俺の気まずさが。そんなにいいもんじゃないだろう？</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>一応の礼儀として挨拶してみるが、返事がないどころかこっちをチラリとも見やしない。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>セーラー服姿の桐乃は、ソファに深く腰掛け、超短いスカートで足を組み、携帯に向かって何やら楽しそうにけらけら笑いを振りまいている。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>その笑顔はなるほどかわいかったが、それが俺に向けられることは今後もないだろう。</p>"
+},
+{
+	"message":"<div class='comment-left'><img class='comment-left' alt='高坂桐乃' src='./img/img_icon_profile_sister@3x.png'><img class='comment-left animated bounceIn'alt='えー？　ウッソー？　なにそれぇ。きゃはは、ばっかみたーい' src='./img/img_ballon_serif_03@3x.png'></div>"
+},
+{
+	"message":"<p class='animated fadeInUp'>ああ、おまえなんかに話しかけた俺がバカだったよ。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>俺は心の中で毒づいて、ぱかんと冷蔵庫を開けた。パックの麦茶を取り出し、グラスに注いで一気にあおる。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>ふぅ、とひと心地ついてから、その場を後にした。</p>"
+},
+{
+	"message":"<div class='comment-left'><img class='comment-left' alt='高坂桐乃' src='./img/img_icon_profile_sister@3x.png'><img class='comment-left animated bounceIn'alt='うん、うん……分かった。じゃあ着替えて、これから行くね──' src='./img/img_ballon_serif_04@3x.png'></div>"
+},
+{
+	"message":"<p class='animated fadeInUp'>もう夕方になるってのに、どこに遊びに行くのやら。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>まあ、しょせん俺には関係のない話だけどな。俺は心の中で呟いて、階段を上っていった。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>俺の名前は、高坂京介。近所の高校に通う十七歳。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>自分でいうのもなんだが、ごく平凡な男子高校生である。所属している部活はないし、趣味も特筆するようなもんはない。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>そりゃ流行の音楽くらいは聴くし、漫画やら小説だって、まあそれなりには読むけど、趣味といえるほどのもんじゃないな。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>放課後はだいたい友達と町をぶらつきながらだべったり、家で漫画読んだり、テレビ見たり。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>ときにはまぁ……勉強したりもする。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>だいたい普通の高校生ってのはそんなもんだろう？</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>無難でつまらない毎日だと言われるかもしれないが、『普通』でいるってのは、わりと大事なもんだと俺は思う。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>普通っていうのは、周りと足並み揃えて、地に足つけて生きるってことで。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>無難ってのは危険が少ないってことだ。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>幸い俺の成績は、いまのところ悪かあない。このまま順調にいけば、わりといい大学に進学できるんじゃないかと思う。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>その先、将来どうするか──なんてのは、四年間のキャンパスライフを楽しみながら、ゆっくりと考えればいいことだ。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>いまから慌てなきゃならないのは、そのやり方では就けない職業を目指しているやつらくらいのもんだろう。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>夢を追いかける──聞こえはいいけどな。それは『普通』じゃなくなるってことだ。危険は多いし、間違っても無難じゃない。少なくとも俺には向いてないね。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>ま、子供の頃の夢なんて、とっくの昔に忘れちまったけど……強いて言うなら。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>平々凡々、目立たず騒がず穏やかに、のんびりまったり生きていくのが俺の夢ってところかな。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>我が家は二階建ての一軒家。家族構成は俺と妹と、それに両親の四人。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>そこそこ裕福な、別段珍しくもない、ありふれた家庭。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>俺と妹の部屋は二階にある。部屋で私服に着替えた俺は、十分ほどくつろいでから階段を下りた。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>勉強を始める前に、トイレを済ませておこうと思ったからだ。ちなみに階段を下りるとすぐ玄関で、向かって左手にリビングの扉がある。</p>"
+},
+{
+	"message":"<div class='comment-right'><img class='comment-right' alt='高坂京介' src='./img/img_icon_profile_brother@3x.png'><img class='comment-right animated bounce' alt='っと' src='./img/img_ballon_serif_05@3x.png'></div>"
+},
+{
+	"message":"<p class='animated fadeInUp'>階段を下りてすぐ、玄関付近で、私服の妹とぶつかった。実はこの位置、お互いにとって死角になるので、接触事故が多発するポイントなのだ。</p>"
+},
+{
+	"message":"<div class='comment-center animated bounceIn'><img class='comment-center' alt='どん。' src='./img/img_ballon_serif_06@3x.png'></div>"
+},
+{
+	"message":"<p class='animated fadeInUp'>俺の左肩が桐乃の胸にぶつかるような形で、軽く衝突。</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>衝撃自体はたいしたことがなかったのだが、その拍子に妹のバッグが手から離れ、床に中身をぶちまけた。</p>"
+},
+{
+	"message":"<div class='comment-left'><img class='comment-left' alt='高坂桐乃' src='./img/img_icon_profile_sister@3x.png'><img class='comment-left animated tada'alt='あっ……' src='./img/img_ballon_serif_07@3x.png'></div>"
+},
+{
+	"message":"<div class='comment-right'><img class='comment-right' alt='高坂京介' src='./img/img_icon_profile_brother@3x.png'><img class='comment-right animated slideInUp' alt='お、悪い' src='./img/img_ballon_serif_08@3x.png'></div>"
+},
+{
+	"message":"<p class='animated fadeInUp'>俺は素直に詫びて、床に散らばった化粧品等の諸々に手を伸ばそうとしたのだが……</p>"
+},
+{
+	"message":"<p class='animated fadeInUp'>ぱしっ。それを察した桐乃が、俺の手を平手で払った。</p>"
+},
+{
+	"message":"<div class='comment-right'><img class='comment-right' alt='高坂京介' src='./img/img_icon_profile_brother@3x.png'><img class='comment-right animated tada' alt='なっ' src='./img/img_ballon_serif_09@3x.png'></div>"
+},
+{
+	"message":"<p class='animated fadeInUp'>目を見開いた俺は、鋭い視線を向けられて絶句する。</p>"
+},
+{
+	"message":"<img alt='artwork' src='./img/zoomimage.png' class='artwork' id='artwork' data-lity>"
+},
+{
+	"message":"<p class='animated fadeInUp'>妹の口から出た台詞はこうだ。</p>"
+},
+{
+	"message":"<div class='comment-left'><img class='comment-left' alt='高坂桐乃' src='./img/img_icon_profile_sister@3x.png'><img class='comment-left animated tada'alt='いいから、さわんないで' src='./img/img_ballon_serif_10@3x.png'style='height:5rem;top:35px;'></div>"
+},
+{
+	"message":"<img alt='finish' src='./img/read_done@3x.png' class='finish-img' id='finish'>"
+}
+]
+
+};
+
+
+//count用
+var count = 0;
+var root = 'novel';
+//一時停止カウント
+var stop = false;
+var count2 =0;
+var timerID;
+
+//タップで一時停止処理（指を離した時に実行）
+window.addEventListener('touchend', function(event) {
+	if(count>0){
+		if(stop===false){
+	  		console.log('pause');
+	  		//アイコン表示
+	  		$("#pause").fadeIn();
+	  		setTimeout('$("#pause").fadeOut()',500);
+	  		stop = true;
+		}else if(stop===true){
+			//スクロールを止めた時にアイコンが表示されないように
+			if(count2!=2){
+				console.log('play');
+				//アイコン表示
+				$("#play").fadeIn();
+		  		setTimeout('$("#play").fadeOut()',500);
+			}
+			
+	  		stop = false;
+	  		clearTimeout(timerID);
+			timerID = setTimeout(function(){
+			if(allroot[root][count-1].time===null){
+				add();
+			}else{
+				setTimeout('add()',allroot[root][count-1].time);
+			}
+			//配列にtimeを作成し、それが定義された時のみ時間間隔を操作する
+			},3000);
+			}
+	}
+	count2 = 1;
+}, false);
+
+//スクロール中は自動で文章を挿入しない
+window.addEventListener('touchmove', function(event) {
+	console.log('scroll');
+	stop = true;
+	//再開時にアイコンを出さないように
+	count2 = 2;
+}, false);
+
+function add(){
+
+	clearTimeout(timerID);
+	if(stop===true){
+		return;
+	}
+	count++;
+	    var parent_object = document.getElementById("wrapper");
+	    //1文目が画面上部にくるように
+	    if(count==1){
+	    	parent_object.style.minHeight="100vh";
+	    }
+
+	    var div_element = document.createElement("div");
+	    div_element.innerHTML = '<div class="divload" id="divload">'+allroot[root][count-1].message+'</div>';
+	
+	    //var parent_object = document.getElementById("wrapper");
+	    parent_object.appendChild(div_element);
+
+        document.getElementById("button_start").style.display="none";
+
+	if(count==58){//漫画を表示する時
+	//１文の読み込み速度
+		var element = document.getElementById( "artwork" ) ;
+		var rect = element.getBoundingClientRect() ;
+		var positionY = rect.top + window.pageYOffset ;	
+		window.scrollTo( 0, positionY ) ;
+	}else{
+		window.scrollTo( 0, count*1000 ) ;//追加したら一番下にスクロール
+	}
+	//１文の読み込み速度
+	if(count2 = 1){
+		clearTimeout(timerID);
+		timerID = setTimeout(function(){
+			if(allroot[root][count-1].time===null){
+				add();
+			}else{
+				setTimeout('add()',allroot[root][count-1].time);
+			}
+			//配列にtimeを作成し、それが定義された時のみ時間間隔を操作する
+		},3000);
+	}
+	window.scrollTo( 0, count*10000 ) ;//追加したら一番下にスクロール
+
+}
+
